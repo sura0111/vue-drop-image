ccd262297561ac942b14680ce72ea6a494aa0c95
sura.kh <sura.kh@rakuten.com>
2021-12-07 09:55:09 +0900

    refactor: remove some config

--- dist/types/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as DropFile } from './dropFile.vue';
-export { default as DropImage } from './dropImage.vue';
-export { default as LoadFile } from './loadFile.vue';
-export { default as LoadImage } from './loadImage.vue';
